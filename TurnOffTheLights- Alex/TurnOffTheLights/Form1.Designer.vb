ac4653150ff6e8fafeddc03849d486601f37b624
AlexMassin <aptmassin@gmail.com>
2019-03-08 13:43:31 -0500

    Initial Commit

---
+++ TurnOffTheLights- Alex/TurnOffTheLights/Form1.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnPhantom = New System.Windows.Forms.Button()
+        Me.btnEarthshaker = New System.Windows.Forms.Button()
+        Me.btnDisruptor = New System.Windows.Forms.Button()
+        Me.btnOgre = New System.Windows.Forms.Button()
+        Me.btnClockwork = New System.Windows.Forms.Button()
+        Me.btnNecro = New System.Windows.Forms.Button()
+        Me.btnAxe = New System.Windows.Forms.Button()
+        Me.btnZeus = New System.Windows.Forms.Button()
+        Me.btnInvoker = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnPhantom
+        '
+        Me.btnPhantom.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btnPhantom.BackgroundImage = Global.TurnOffTheLights.My.Resources.Resources.Phantom
+        Me.btnPhantom.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnPhantom.Location = New System.Drawing.Point(23, 31)
+        Me.btnPhantom.Name = "btnPhantom"
+        Me.btnPhantom.Size = New System.Drawing.Size(177, 173)
+        Me.btnPhantom.TabIndex = 0
+        Me.btnPhantom.UseVisualStyleBackColor = False
+        '
+        'btnEarthshaker
+        '
+        Me.btnEarthshaker.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btnEarthshaker.BackgroundImage = Global.TurnOffTheLights.My.Resources.Resources.Earthshaker
+        Me.btnEarthshaker.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnEarthshaker.Location = New System.Drawing.Point(206, 389)
+        Me.btnEarthshaker.Name = "btnEarthshaker"
+        Me.btnEarthshaker.Size = New System.Drawing.Size(177, 173)
+        Me.btnEarthshaker.TabIndex = 1
+        Me.btnEarthshaker.UseVisualStyleBackColor = False
+        '
+        'btnDisruptor
+        '
+        Me.btnDisruptor.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btnDisruptor.BackgroundImage = Global.TurnOffTheLights.My.Resources.Resources.Disruptor
+        Me.btnDisruptor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnDisruptor.Location = New System.Drawing.Point(23, 389)
+        Me.btnDisruptor.Name = "btnDisruptor"
+        Me.btnDisruptor.Size = New System.Drawing.Size(177, 173)
+        Me.btnDisruptor.TabIndex = 2
+        Me.btnDisruptor.UseVisualStyleBackColor = False
+        '
+        'btnOgre
+        '
+        Me.btnOgre.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btnOgre.BackgroundImage = Global.TurnOffTheLights.My.Resources.Resources.Ogre
+        Me.btnOgre.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnOgre.Location = New System.Drawing.Point(389, 210)
+        Me.btnOgre.Name = "btnOgre"
+        Me.btnOgre.Size = New System.Drawing.Size(177, 173)
+        Me.btnOgre.TabIndex = 3
+        Me.btnOgre.UseVisualStyleBackColor = False
+        '
+        'btnClockwork
+        '
+        Me.btnClockwork.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btnClockwork.BackgroundImage = Global.TurnOffTheLights.My.Resources.Resources.Clockwork
+        Me.btnClockwork.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnClockwork.Location = New System.Drawing.Point(206, 210)
+        Me.btnClockwork.Name = "btnClockwork"
+        Me.btnClockwork.Size = New System.Drawing.Size(177, 173)
+        Me.btnClockwork.TabIndex = 4
+        Me.btnClockwork.UseVisualStyleBackColor = False
+        '
+        'btnNecro
+        '
+        Me.btnNecro.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btnNecro.BackgroundImage = Global.TurnOffTheLights.My.Resources.Resources.Necro
+        Me.btnNecro.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnNecro.Location = New System.Drawing.Point(23, 210)
+        Me.btnNecro.Name = "btnNecro"
+        Me.btnNecro.Size = New System.Drawing.Size(177, 173)
+        Me.btnNecro.TabIndex = 5
+        Me.btnNecro.UseVisualStyleBackColor = False
+        '
+        'btnAxe
+        '
+        Me.btnAxe.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btnAxe.BackgroundImage = Global.TurnOffTheLights.My.Resources.Resources.Axe
+        Me.btnAxe.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnAxe.Location = New System.Drawing.Point(389, 31)
+        Me.btnAxe.Name = "btnAxe"
+        Me.btnAxe.Size = New System.Drawing.Size(177, 173)
+        Me.btnAxe.TabIndex = 6
+        Me.btnAxe.UseVisualStyleBackColor = False
+        '
+        'btnZeus
+        '
+        Me.btnZeus.BackColor = System.Drawing.Color.Black
+        Me.btnZeus.BackgroundImage = Global.TurnOffTheLights.My.Resources.Resources.Zeus
+        Me.btnZeus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnZeus.Location = New System.Drawing.Point(206, 31)
+        Me.btnZeus.Name = "btnZeus"
+        Me.btnZeus.Size = New System.Drawing.Size(177, 173)
+        Me.btnZeus.TabIndex = 7
+        Me.btnZeus.UseVisualStyleBackColor = False
+        '
+        'btnInvoker
+        '
+        Me.btnInvoker.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btnInvoker.BackgroundImage = Global.TurnOffTheLights.My.Resources.Resources.Invoker
+        Me.btnInvoker.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnInvoker.Location = New System.Drawing.Point(389, 389)
+        Me.btnInvoker.Name = "btnInvoker"
+        Me.btnInvoker.Size = New System.Drawing.Size(177, 173)
+        Me.btnInvoker.TabIndex = 8
+        Me.btnInvoker.UseVisualStyleBackColor = False
+        '
+        'btnReset
+        '
+        Me.btnReset.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.btnReset.Font = New System.Drawing.Font("Impact", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnReset.Location = New System.Drawing.Point(600, 166)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(145, 55)
+        Me.btnReset.TabIndex = 9
+        Me.btnReset.Text = "RESET"
+        Me.btnReset.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.btnExit.Font = New System.Drawing.Font("Impact", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnExit.Location = New System.Drawing.Point(670, 505)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 57)
+        Me.btnExit.TabIndex = 10
+        Me.btnExit.Text = "EXIT"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.TurnOffTheLights.My.Resources.Resources.Dota3
+        Me.ClientSize = New System.Drawing.Size(780, 617)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnInvoker)
+        Me.Controls.Add(Me.btnZeus)
+        Me.Controls.Add(Me.btnAxe)
+        Me.Controls.Add(Me.btnNecro)
+        Me.Controls.Add(Me.btnClockwork)
+        Me.Controls.Add(Me.btnOgre)
+        Me.Controls.Add(Me.btnDisruptor)
+        Me.Controls.Add(Me.btnEarthshaker)
+        Me.Controls.Add(Me.btnPhantom)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnPhantom As System.Windows.Forms.Button
+    Friend WithEvents btnEarthshaker As System.Windows.Forms.Button
+    Friend WithEvents btnDisruptor As System.Windows.Forms.Button
+    Friend WithEvents btnOgre As System.Windows.Forms.Button
+    Friend WithEvents btnClockwork As System.Windows.Forms.Button
+    Friend WithEvents btnNecro As System.Windows.Forms.Button
+    Friend WithEvents btnAxe As System.Windows.Forms.Button
+    Friend WithEvents btnZeus As System.Windows.Forms.Button
+    Friend WithEvents btnInvoker As System.Windows.Forms.Button
+    Friend WithEvents btnReset As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ TurnOffTheLights- Alex/TurnOffTheLights/Form1.vb
@@ -0,0 +1,242 @@
+﻿Public Class Form1
+    '
+    '
+    ' (  )   /\   _                  (     
+    '\ |  (  \ ( \.(                  )                      _____
+    '\  \ \  `  `   ) \              (  ___                 / _   \
+    '(_`    \+   . x  ( .\            \/   \____-----------/ (o)   \_
+    '- .-               \+  ;         (  O                           \____
+    '  Alexander Massin       )        \_____________  `              \  /
+    '(__    11a      +- .( -'.- <. - _   VVVVVVV VV V\                 \/
+    '(_____            ._._: <_ - <- _ (--  _AAAAAAA__A_/                  |
+    ' .    /./.+-  . .- /  +--  - .     \______________//_              \_______
+    ' (__ ' /x  / x _/ (                                  \___'          \     /
+    ' , x / ( '  . / .  /                                     |           \   /
+    ' /  /  _/ /    +                                        /              \/
+    '   (__/                                              /                  \
+    '
+    '
+    '
+    Private Sub btnExit_Click(sender As System.Object, e As System.EventArgs) Handles btnExit.Click
+        Application.Exit() 'This is how the user can exit the game'
+    End Sub
+
+    Private Sub btnPhantom_Click(sender As System.Object, e As System.EventArgs) Handles btnPhantom.Click
+        btnPhantom.Enabled = False
+        btnPhantom.Visible = False
+        If btnZeus.Enabled = False Then          'Some patterns to finish quick include: C+G+A+I+E OR C+I+G+A+E OR E+C+A+I+G OR B+F+H+D+C+A+I+G+E+B+D+F+HN
+            btnZeus.Enabled = True               '    | A | B | C |          
+            btnZeus.Visible = True               '    | D | E | F |
+        Else                                     '    | G | H | I |
+            btnZeus.Enabled = False              'This whole thing is basically saying if the box is enabled, disable it and if its disabled, enable it.
+            btnZeus.Visible = False              'Very tedious
+        End If                                   'This goes on for the next 8 buttons
+        If btnNecro.Enabled = False Then
+            btnNecro.Enabled = True
+            btnNecro.Visible = True
+        Else
+            btnNecro.Enabled = False
+            btnNecro.Visible = False
+        End If
+
+    End Sub
+
+    Private Sub btnAxe_Click(sender As System.Object, e As System.EventArgs) Handles btnAxe.Click
+        btnAxe.Enabled = False
+        btnAxe.Visible = False
+        If btnZeus.Enabled = False Then
+            btnZeus.Enabled = True
+            btnZeus.Visible = True
+        Else
+            btnZeus.Enabled = False                   'Every box only disables and enables the other boxes that are touching it
+            btnZeus.Visible = False
+        End If
+        If btnOgre.Enabled = False Then
+            btnOgre.Enabled = True
+            btnOgre.Visible = True
+        Else
+            btnOgre.Enabled = False
+            btnOgre.Visible = False
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As System.Object, e As System.EventArgs) Handles btnReset.Click
+        Application.Restart()            'This is how the user restarts the program by clicking the reset button
+    End Sub
+
+    Private Sub btnZeus_Click(sender As System.Object, e As System.EventArgs) Handles btnZeus.Click
+        btnZeus.Enabled = False
+        btnZeus.Visible = False
+        If btnPhantom.Enabled = False Then
+            btnPhantom.Enabled = True
+            btnPhantom.Visible = True
+        Else
+            btnPhantom.Enabled = False
+            btnPhantom.Visible = False
+        End If
+        If btnAxe.Enabled = False Then
+            btnAxe.Enabled = True
+            btnAxe.Visible = True
+        Else
+            btnAxe.Enabled = False
+            btnAxe.Visible = False
+        End If
+        If btnClockwork.Enabled = False Then
+            btnClockwork.Enabled = True
+            btnClockwork.Visible = True
+        Else
+            btnClockwork.Enabled = False
+            btnClockwork.Visible = False
+        End If
+    End Sub
+
+    Private Sub btnClockwork_Click(sender As System.Object, e As System.EventArgs) Handles btnClockwork.Click
+        btnClockwork.Enabled = False
+        btnClockwork.Visible = False
+        If btnOgre.Enabled = False Then
+            btnOgre.Enabled = True
+            btnOgre.Visible = True
+        Else
+            btnOgre.Enabled = False
+            btnOgre.Visible = False
+        End If
+        If btnZeus.Enabled = False Then                    'This is a bigger code than the other buttons because it touches more buttons
+            btnZeus.Enabled = True
+            btnZeus.Visible = True
+        Else
+            btnZeus.Enabled = False
+            btnZeus.Visible = False
+        End If
+        If btnEarthshaker.Enabled = False Then
+            btnEarthshaker.Enabled = True
+            btnEarthshaker.Visible = True
+        Else
+            btnEarthshaker.Enabled = False
+            btnEarthshaker.Visible = False
+        End If
+        If btnNecro.Enabled = False Then
+            btnNecro.Enabled = True
+            btnNecro.Visible = True
+        Else
+            btnNecro.Enabled = False
+            btnNecro.Visible = False
+        End If
+    End Sub
+
+    Private Sub btnNecro_Click(sender As System.Object, e As System.EventArgs) Handles btnNecro.Click
+        btnNecro.Enabled = False
+        btnNecro.Visible = False
+        If btnClockwork.Enabled = False Then
+            btnClockwork.Enabled = True
+            btnClockwork.Visible = True
+        Else
+            btnClockwork.Enabled = False
+            btnClockwork.Visible = False
+        End If
+        If btnPhantom.Enabled = False Then
+            btnPhantom.Enabled = True
+            btnPhantom.Visible = True
+        Else
+            btnPhantom.Enabled = False
+            btnPhantom.Visible = False
+        End If
+        If btnDisruptor.Enabled = False Then
+            btnDisruptor.Enabled = True
+            btnDisruptor.Visible = True
+        Else
+            btnDisruptor.Enabled = False
+            btnDisruptor.Visible = False
+        End If
+    End Sub
+
+    Private Sub btnOgre_Click(sender As System.Object, e As System.EventArgs) Handles btnOgre.Click
+        btnOgre.Enabled = False
+        btnOgre.Visible = False
+        If btnInvoker.Enabled = False Then
+            btnInvoker.Enabled = True
+            btnInvoker.Visible = True
+        Else
+            btnInvoker.Enabled = False
+            btnInvoker.Visible = False
+        End If
+        If btnAxe.Enabled = False Then
+            btnAxe.Enabled = True
+            btnAxe.Visible = True
+        Else
+            btnAxe.Enabled = False
+            btnAxe.Visible = False
+        End If
+        If btnClockwork.Enabled = False Then
+            btnClockwork.Enabled = True
+            btnClockwork.Visible = True
+        Else
+            btnClockwork.Enabled = False
+            btnClockwork.Visible = False
+        End If
+
+    End Sub
+
+    Private Sub btnEarthshaker_Click(sender As System.Object, e As System.EventArgs) Handles btnEarthshaker.Click
+        btnEarthshaker.Enabled = False
+        btnEarthshaker.Visible = False
+        If btnClockwork.Enabled = False Then
+            btnClockwork.Enabled = True
+            btnClockwork.Visible = True
+        Else
+            btnClockwork.Enabled = False
+            btnClockwork.Visible = False
+        End If
+        If btnInvoker.Enabled = False Then
+            btnInvoker.Enabled = True
+            btnInvoker.Visible = True
+        Else
+            btnInvoker.Enabled = False
+            btnInvoker.Visible = False
+        End If
+        If btnDisruptor.Enabled = False Then
+            btnDisruptor.Enabled = True
+            btnDisruptor.Visible = True
+        Else
+            btnDisruptor.Enabled = False
+            btnDisruptor.Visible = False
+        End If
+    End Sub
+
+    Private Sub btnInvoker_Click(sender As System.Object, e As System.EventArgs) Handles btnInvoker.Click
+        btnInvoker.Enabled = False
+        btnInvoker.Visible = False
+        If btnEarthshaker.Enabled = False Then
+            btnEarthshaker.Enabled = True
+            btnEarthshaker.Visible = True
+        Else
+            btnEarthshaker.Enabled = False
+            btnEarthshaker.Visible = False
+        End If
+        If btnOgre.Enabled = False Then
+            btnOgre.Enabled = True
+            btnOgre.Visible = True
+        Else
+            btnOgre.Enabled = False
+            btnOgre.Visible = False
+        End If
+    End Sub
+
+    Private Sub btnDisruptor_Click(sender As System.Object, e As System.EventArgs) Handles btnDisruptor.Click
+        btnDisruptor.Enabled = False
+        btnDisruptor.Visible = False
+        If btnNecro.Enabled = False Then
+            btnNecro.Enabled = True
+            btnNecro.Visible = True
+        Else
+            btnNecro.Enabled = False
+            btnNecro.Visible = False
+        End If
+        If btnEarthshaker.Enabled = False Then
+            btnEarthshaker.Enabled = True
+            btnEarthshaker.Visible = True
+        Else
+            btnEarthshaker.Enabled = False
+            btnEarthshaker.Visible = False
+        End If
+    End Sub
+End Class
